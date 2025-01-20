cda68d857dad997495d28f2689f8fc977a50c04e
andbrandt <anders.github@hjemmebrandt.com>
2025-01-20 08:33:53 +0100

    Box design and all laser files completed. First iteration of RealFlightBasic bat manipulation.

--- sim_pc/RealFlightBasic/Program Files (x86)/RealFlightBasic/RealFlight.bat
+++ sim_pc/RealFlightBasic/Program Files (x86)/RealFlightBasic/RealFlight.bat
@@ -1,5 +1,6 @@
-// SimTimer MUST signal [LEFT_ARROW] [LEFT_ARROW] [LEFT_ARROW] [Alt+F4] before calling this bat file
-// SimTimer MUST signal [WIN] "c:\Program Files (x86)\RealFlightBasic\RealFlight.bat" [RETURN] to invoke this bat file
+// SimTimer MUST signal [LEFT_ARROW] [LEFT_ARROW] [LEFT_ARROW] [Alt+F4] before invoking this bat file
+// To invoke this bat file SimTimer MUST signal 
 
 copy RealFlightBasic_simTimerStart.ini "C:\Users\%USERNAME%\Documents\RealFlight Basic\RealFlightBasic.ini"
-"c:\Program Files (x86)\RealFlightBasic\RealFlight.exe"
+RealFlight.exe
+
